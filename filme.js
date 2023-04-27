7b90f4c38d9aa76407520987e5d21ecb22eaabe6
Amandadevnunes <aluno103.23187368amanda@gmail.com>
2023-04-26 22:02:51 -0300

    peimeiro commit

---
+++ filme.js
@@ -0,0 +1,12 @@
+const formfilme = document.getElementById("form-filme"); 
+const valores = [number1, nunber2, nunber3, number4];
+
+let nota = []; 
+
+
+const nunber1 = document.getElementById("bt1").value; 
+const nunber2 = document.getElementById("bt2").value;
+const nunber3 = document.getElementById("bt3").value;
+const nunber4 = document.getElementById("bt4").value;
+
+const total = [`> valores` ]; 
