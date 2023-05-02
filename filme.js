ecb98676574640606528327459a21182ec1b3116
Amandadevnunes <aluno103.23187368amanda@gmail.com>
2023-05-02 19:11:07 -0300

    informações

--- filme.js
+++ filme.js
@@ -1,12 +1,40 @@
-const formfilme = document.getElementById("form-filme"); 
-const valores = [number1, nunber2, nunber3, number4];
+const formfilme = document.getElementById("formfilme");
+//const valores = [number1, nunber2, nunber3, number4];
 
-let nota = []; 
+let filmes = ['f1', 'f2', 'f3', 'f4'];
+
+let nota = [[], [], [], []];
 
 
-const nunber1 = document.getElementById("bt1").value; 
-const nunber2 = document.getElementById("bt2").value;
-const nunber3 = document.getElementById("bt3").value;
-const nunber4 = document.getElementById("bt4").value;
+function darNota() {
 
-const total = [`> valores` ]; 
+    let filme = document.getElementById('filmes').value;
+    let nota = parseFloat(document.getElementById('nota').value);
+
+
+    //const f1 = document.getElementById("nota").value;
+    //alert(nota=f1 + nota);
+
+    //const f2 = document.getElementById("nota").value;
+   // alert(nota=f2 + nota);
+
+   // const f3 = document.getElementById("nota").value;
+   //alert(nota=f3  + nota);
+
+   // const f4 = document.getElementById("nota").value;
+   // alert(nota=f4 + nota);
+  
+
+
+    if (nota >= 0 && nota <= 10) {
+        nota[filmes - 1].push(nota);
+
+        alert (filme == nota);
+
+
+    //alert("o filme" + filmes[filmes - 1] + notas);
+
+    } else {
+       alert(" A nota deve estar entre 0 e 10");
+   }
+}
